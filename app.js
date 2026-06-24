d386b0c8b28446d7ba60a5a6fa7dc1bfecd6abbf
Ebu3v <chukwudebeluchukwuebuka17@gmail.com>
2026-06-24 18:55:20 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
-
+app.set('trust proxy', 1);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
